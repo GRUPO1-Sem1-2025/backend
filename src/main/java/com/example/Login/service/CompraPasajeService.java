0b292e23138cc19e23ae7a4927b62918f5c8209b
Eduardo <eduflores08@gmail.com>
2025-05-03 13:58:21 -0300

    Comentarios
    se borran comentarios

--- src/main/java/com/example/Login/service/CompraPasajeService.java
+++ src/main/java/com/example/Login/service/CompraPasajeService.java
@@ -41,57 +41,6 @@ public class CompraPasajeService {
 	@Autowired
 	private ViajeRepository viajeRepository;
 
-//	public void comprarPasaje(DtoCompraPasaje request) {
-//		Usuario vendedor = new Usuario();
-//		Usuario usuario = usuarioRepository.findById(request.getUsuarioId())
-//				.orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-//		try {
-//			Optional<Usuario> Ovendedor = usuarioRepository.findById(request.getVendedorId());
-//			vendedor = Ovendedor.get();
-//		} catch (Exception e) {
-//			vendedor = null;
-//		}
-//
-//		Optional<Viaje> Oviaje = viajeRepository.findById(request.getViajeId());
-//		Viaje viaje = Oviaje.get();
-//
-//		CompraPasaje compra = new CompraPasaje();
-//		if (vendedor == null) {
-//			compra.setTipo_venta("Online");
-//		} else {
-//			compra.setTipo_venta("Ventanilla");
-//		}
-//		compra.setUsuario(usuario);
-//		compra.setVendedor(vendedor);
-//		compra.setViaje(viaje);
-//		compra.setFechaHoraCompra(LocalDateTime.now());
-//
-//		List<AsientoPorViaje> asientosReservados = new ArrayList<>();
-//
-//		for (Integer nroAsiento : request.getNumerosDeAsiento()) {
-//			Optional<AsientoPorViaje> apvOpt = asientoPorViajeRepository.findByViajeIdAndNroAsiento(viaje.getId(),
-//					nroAsiento);
-//			if (apvOpt.isPresent()) {
-//				AsientoPorViaje apv = apvOpt.get();
-//				if (!apv.isReservado()) {
-//					apv.setReservado(true);
-//					asientosReservados.add(apv);
-//				} else {
-//					throw new RuntimeException("Asiento nro " + nroAsiento + " ya está reservado.");
-//				}
-//			}
-//		}
-//		compra.setCat_pasajes(asientosReservados.size());
-//		compra.setTotal(compra.getCat_pasajes() * viaje.getPrecio());
-//		compra.setAsientos(asientosReservados);
-//		compra.setEstadoCompra("Realizada");
-//		compraPasajeRepository.save(compra);
-//
-//		for (AsientoPorViaje apv : asientosReservados) {
-//			asientoPorViajeRepository.save(apv);
-//		}
-//	}
-
 	public int comprarPasaje(DtoCompraPasaje request) {
 		Usuario vendedor = new Usuario();
 		Usuario usuario = new Usuario();
