20f4360686d4f974947aeb08f9b89b471e9d3d28
Eduardo <eduflores08@gmail.com>
2025-05-15 18:29:08 -0300

    DestinosMasVistos

---
+++ src/main/java/com/example/Login/dto/DtoDestinoMasVistos.java
@@ -0,0 +1,36 @@
+package com.example.Login.dto;
+
+public class DtoDestinoMasVistos {
+	private Long id;
+	private String nombre;
+	private Long cantidad;
+
+	public DtoDestinoMasVistos() {
+		
+	}
+	public DtoDestinoMasVistos(Long id, String nombre, Long cantidad) {
+		this.id = id;
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Long getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(Long cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	
+}
